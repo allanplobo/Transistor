351cb0d1579f2ca2e2928f28e72f3b95aad9eb36
Allan Lobo <allanplobo@gmail.com>
2020-08-04 09:28:06 -0300

    Finaliza segundo conteudo

--- src/pages/Home/SegundoConteudo/index.js
+++ src/pages/Home/SegundoConteudo/index.js
@@ -13,26 +13,28 @@ function SegundoConteudo() {
 
       <div className="DivCards">
         <div className="Cards">
+          {/* <div className="DivCardImage"> */}
           <img
             src={PrimeiroCard}
             className="CardImage"
             alt="Primeiro card"
           ></img>
-          <p className="CardTxt">
-            A Camerata foi apenas os dois no início, e suas fileiras nunca foram
-            destinadas a exceder um número a ser contado em uma mão.
-          </p>
         </div>
+        <p className="CardTxt">
+          A Camerata foi apenas os dois no início, e suas fileiras nunca foram
+          destinadas a exceder um número a ser contado em uma mão.
+        </p>
+        {/* </div> */}
       </div>
       <div className="DivCardCentral">
         <div className="Cards">
           <img src={SegundoCard} className="CardImage" alt="Segundo card"></img>
-          <p className="CardTxt">
-            Red, uma jovem cantora, entrou em posse do Transistor. Sendo a
-            poderosa espada falante. O grupo Possessores quer tanto ela quanto o
-            Transistor e está perseguindo implacavelmente a sua procura.
-          </p>
         </div>
+        <p className="CardTxt">
+          Red, uma jovem cantora, entrou em posse do Transistor. Sendo a
+          poderosa espada falante. O grupo Possessores quer tanto ela quanto o
+          Transistor e está perseguindo implacavelmente a sua procura.
+        </p>
       </div>
       <div className="DivCards">
         <div className="Cards">
@@ -41,11 +43,11 @@ function SegundoConteudo() {
             className="CardImage"
             alt="Terceiro card"
           ></img>
-          <p className="CardTxt">
-            Sybil é descrita pelo Transistor como sendo os "olhos e ouvidos" da
-            Camerata.
-          </p>
         </div>
+        <p className="CardTxt">
+          Sybil é descrita pelo Transistor como sendo os "olhos e ouvidos" da
+          Camerata.
+        </p>
       </div>
 
       <a className="RightArrowCards">
